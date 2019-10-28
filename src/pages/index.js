f6316ede714366ee2017e425954111c947720de0
phatvt <vtphat91@gmail.com>
2019-10-28 09:47:51 +0700

    change default homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,89 +1,51 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { useStaticQuery ,graphql } from 'gatsby'
 import get from 'lodash/get'
-import Helmet from 'react-helmet'
-import Hero from '../components/hero'
 import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import './about-us.css'
 
-class RootIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+const IndexPage = () => {
 
-    return (
-      <Layout location={this.props.location} >
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </div>
-      </Layout>
-    )
-  }
-}
-
-export default RootIndex
-
-export const pageQuery = graphql`
-  query HomeQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-             ...GatsbyContentfulFluid_tracedSVG
+    const data = useStaticQuery(graphql`
+    {
+        contentfulAboutUs {
+            title
+            body {
+              childMarkdownRemark {
+                html
+              }
             }
           }
-          description {
+        contentfulBoxContentBodyTextNode {
             childMarkdownRemark {
               html
             }
-          }
-        }
-      }
+        } 
     }
-    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
-      edges {
-        node {
-          name
-          shortBio {
-            shortBio
-          }
-          title
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-        }
-      }
-    }
-  }
-`
+    `)
+    const title = get(data, 'contentfulAboutUs.title');
+    const body = get(data, 'contentfulAboutUs.body');
+    const bodyBoxContent = get(data, 'contentfulBoxContentBodyTextNode.childMarkdownRemark');
+    return (
+        <Layout>
+            <section className="content_block clearfix wrapper">
+                <section className="content left ceo">
+                    <h2 className="section-headline">{title}</h2>
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: body.childMarkdownRemark.html,
+                        }}/>
+                </section>  
+                <section className="sidebar right sticky fixed">
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: bodyBoxContent.html,
+                        }}/>
+        
+                </section>      
+            </section>
+        </Layout>
+    );
+}
+
+export default IndexPage;
