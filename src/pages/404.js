4ac4a7b7333a71b3fc65f5c95f327ab764fb4560
phatvt <vtphat91@gmail.com>
2019-10-28 09:41:59 +0700

    add product content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,13 @@
 import React from "react"
 
+import Layout from "../components/layout"
 
 
-const NotFoundPage = () => {
+const NotFoundPage1 = () => {
   if (typeof window !== 'undefined') {
     window.location = '/about-us';
   }
   return null;
 }
 
-export default NotFoundPage
+export default NotFoundPage1
